89b745292d3cfd926f85ffdc666d41db9178c584
KUAI <2542862169@qq.com>
2022-03-10 21:39:47 +0800

    line-height

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ## svg.js
 将img批量复制为svg
+
+## line-height.js
+将行距为5的段落批量修改为行距2.2
---
+++ line-height.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: KUAI(https://github.com/kuainx)
+ * @Date: 2022-03-10 21:15:59
+ * @LastEditTime: 2022-03-10 21:38:14
+ * @LastEditors: KUAI
+ * @Description:
+ * @FilePath: \wx-js\line-height.js
+ */
+
+(function () {
+  const win = document.getElementById("ueditor_0") ? document.getElementById("ueditor_0").contentWindow : window;
+  const pList = win.document.getElementsByTagName("section");
+  console.log("共有", pList.length, "段落");
+  for (let i = 0; i < pList.length; i++) {
+    const element = pList[i];
+    if (element.style.lineHeight == '5em') {
+      element.style.lineHeight = '2.2em'
+      console.log("已修改第", i + 1, "段落");
+    }
+  }
+})();
