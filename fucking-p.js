e0eb3aa06ef5be07f6cfcc138a8a7e973ceba6f7
unknown <2542862169@qq.com>
2022-06-06 20:00:31 +0800

    0606

---
+++ fucking-p.js
@@ -0,0 +1,5 @@
+(document.getElementById('ueditor_0') ? document.getElementById('ueditor_0').contentWindow.document : document)
+  .querySelectorAll('body>p')
+  .forEach(e => {
+    e.style.marginBottom = '0px';
+  });
--- svg.js
+++ svg.js
@@ -1,7 +1,7 @@
 /*
  * @Author: KUAI(https://github.com/kuainx)
  * @Date: 2022-03-06 12:53:15
- * @LastEditTime: 2022-04-22 20:32:32
+ * @LastEditTime: 2022-05-27 00:14:58
  * @LastEditors: KUAI
  * @Description:
  * @FilePath: \wx-js\svg.js
@@ -12,10 +12,8 @@ function createSvg(src, width, height) {
   <svg space="preserve" style="display: inline-block;vertical-align: top;background-position:0% 0%;background-attachment: scroll;background-repeat: no-repeat;background-size: 100%;background-image: url(&quot;${src}&quot;);-webkit-tap-highlight-color:transparent;width: 100%" version="1.1" viewBox="0 0 ${width} ${height}" x="0px" y="0px"></svg>`;
   return container.children[0];
 }
-let imgList = document.querySelectorAll(".rich_pages.wxw-img");
-if (imgList.length === 0) {
-  imgList = document.getElementById("ueditor_0").contentWindow.document.querySelectorAll(".rich_pages.wxw-img");
-}
+const win = document.getElementById('ueditor_0') ? document.getElementById('ueditor_0').contentWindow : window;
+imgList = win.document.querySelectorAll('img');
 console.log('共有', imgList.length, '图片');
 let i = 0;
 imgList.forEach(element => {
@@ -31,5 +29,5 @@ imgList.forEach(element => {
     svgDom.style.height = element.style.height;
   }
   element.parentElement.insertBefore(svgDom, element);
-  console.log('已完成第', i, '图片复制', element);
+  console.log('已完成第', i, '图片复制，右键点击元素可以快速跳转或删除', element, svgDom);
 });
